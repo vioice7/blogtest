f566c042f05fabff618537f37fe47a9955ecfaed
Vio <vioice7@yahoo.com>
2021-08-27 16:14:26 +0300

    homepage article test

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,16 +10,16 @@ class ArticleController extends AbstractController
 {
     
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('TEST');
+        return $this->render('article/homepage.html.twig');
     }
 
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
