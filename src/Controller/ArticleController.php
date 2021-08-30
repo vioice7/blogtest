d90045880d7a6bc977b177fd86205558554d35e7
Vio <vioice7@yahoo.com>
2021-08-30 15:20:39 +0300

    cache adapter environment

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Twig\Environment;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class ArticleController extends AbstractController
 {
@@ -25,7 +26,11 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug, Environment $twigEnvironment, MarkdownInterface $markdown)
+    public function show(
+        $slug, Environment $twigEnvironment, 
+        MarkdownInterface $markdown,
+        AdapterInterface $cache
+        )
     {
         
         // dump($slug, $this);
@@ -41,17 +46,28 @@ class ArticleController extends AbstractController
         lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
         labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
         turkey shank eu pork belly meatball non cupim.
+
         Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
         laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
         capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
-        picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
+        picanha shank et filet **mignon** pork belly ut ullamco. Irure velit turducken ground round doner incididunt
         occaecat lorem meatball prosciutto quis strip steak.
+
         Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
         mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
         strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
         cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
         fugiat.
         EOF;
+
+        //dump($markdown);die;
+        //dump($cache);die;
+        $item = $cache->getItem('markdown_'.md5($articleContent));
+        //dump($item);die;
+        if (!$item->isHit()) {
+            $item->set($markdown->transform($articleContent));
+            $cache->save($item);
+        }
 
         $articleContent = $markdown->transform($articleContent);
 
