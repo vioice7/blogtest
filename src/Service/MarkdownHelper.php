ea6779d25ef91c9a468200a272960b658d0227d8
Vio <vioice7@yahoo.com>
2021-08-31 13:52:41 +0300

    cache only on a prod environment

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -14,11 +14,14 @@ class MarkdownHelper
 
     private $logger;
 
-    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger)
+    private $isDebug;
+
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger, bool $isDebug)
     {
         $this->cache = $cache;
         $this->markdown = $markdown;
         $this->logger = $markdownLogger;
+        $this->isDebug = $isDebug;
     }
     
     public function parse(string $source): string
@@ -31,6 +34,11 @@ class MarkdownHelper
             $this->logger->info('They are talking about bacon again!');
         }
         
+        if($this->isDebug)
+        {
+            return $this->markdown->transform($source);
+        }
+
         $item = $this->cache->getItem('markdown_'.md5($source));
         if (!$item->isHit()) {
             $item->set($this->markdown->transform($source));
