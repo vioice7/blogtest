a377acfce9ce4904eec9aa410750bb2cedb7b015
Vio <vioice7@yahoo.com>
2021-09-03 14:33:41 +0300

    timestampable entity doctrine module

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -54,6 +54,18 @@ class Article
      */
     private $imageFilename;
 
+    /**
+     * @ORM\Column(type="datetime")
+     * @Gedmo\Timestampable(on="create")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Gedmo\Timestampable(on="update")
+     */
+    private $updatedAt;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -151,6 +163,30 @@ class Article
     public function incrementHeartCount(): self
     {
         $this->heartCount = $this->heartCount + 1;
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
         return $this;
     }
 }
