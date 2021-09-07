81a20e43ebebc209304119366cc296f24ffdc763
Vio <vioice7@yahoo.com>
2021-09-07 14:40:24 +0300

    comment search admin

--- src/Controller/CommentAdminController.php
+++ src/Controller/CommentAdminController.php
@@ -2,19 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class CommentAdminController extends AbstractController
+class CommentAdminController extends Controller
 {
     /**
      * @Route("/admin/comment", name="comment_admin")
      */
-    public function index(): Response
+    public function index(CommentRepository $repository, Request $request)
     {
+        $q = $request->query->get('q');
+        $comments = $repository->findAllWithSearch($q);
+
         return $this->render('comment_admin/index.html.twig', [
-            'controller_name' => 'CommentAdminController',
+            'comments' => $comments,
         ]);
     }
 }
